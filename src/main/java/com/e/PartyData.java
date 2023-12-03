a386870ecb9a4df82841fc5b626770113a2a0002
bushra-jabbar <bushrajabbar392@gmail.com>
2023-12-03 12:47:02 -0800

    new commit

---
+++ src/main/java/com/e/PartyData.java
@@ -0,0 +1,12 @@
+package com.e;
+public class PartyData {
+    private String year;
+
+    public PartyData(String year) {
+        this.year = year;
+    }
+
+    public String getYear() {
+        return year;
+    }
+}
